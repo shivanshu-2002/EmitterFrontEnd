53f72a12e860c84b49060ac3481961efde30129e
Shivanshu <pshivanshu99@gmail.com>
2024-01-24 15:40:38 +0530

    auth Done

--- src/utils/ApiCall.jsx
+++ src/utils/ApiCall.jsx
@@ -1,32 +1,46 @@
 import axios from "axios";
-// const mainUrl = 'http://localhost:3000/api/v1'
-const mainUrl = 'https://weary-pike-sandals.cyclic.app/api/v1' //for Production
 
+const mainUrl = 'https://weary-pike-sandals.cyclic.app/api/v1';
 
-export const apiCall = async (url,body) => {
-    try {
-       const {data} = await axios.post(
-             `${mainUrl}${url}`,
-           body,
-           { withCredentials: true }
-        );
-        console.log(data)
-        return data;
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+// Function to get the token from localStorage
+const getToken = () => {
+  return localStorage.getItem("token");
+};
+
+// Common configuration for Axios requests with headers
+const axiosConfig = {
+  withCredentials: true,
+  headers: {
+    "Authorization": `Bearer ${getToken()}`,
+    "Content-Type": "application/json",
+  },
+};
+
+export const apiCall = async (url, body) => {
+  try {
+    const { data } = await axios.post(
+      `${mainUrl}${url}`,
+      body,
+      axiosConfig
+    );
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 };
 
 export const apiCallGet = async (url) => {
-    try {
-       const {data} = await axios.get(
-             `${mainUrl}${url}`, { withCredentials: true }
-        );
-        console.log(data)
-        return data;
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  try {
+    const { data } = await axios.get(
+      `${mainUrl}${url}`,
+      axiosConfig
+    );
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 };
